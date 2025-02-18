dee45f79c1d948e6258fc9f04b7207e29620703a
Priyansh Sultania <psultania1000@gmail.com>
2025-02-18 20:37:02 +0530

    styling done, moving on to dynamic pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
 
-const rootDir = require("../util/path");
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("In a middleware which always runs");
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
