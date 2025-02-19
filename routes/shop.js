a71713e4872bd3411fbbcf4f08d2ab47423dbfbe
Priyansh Sultania <psultania1000@gmail.com>
2025-02-19 10:31:11 +0530

    adding template engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,15 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const adminData = require("../routes/admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  console.log("books: ", adminData.products);
 });
 
 module.exports = router;
