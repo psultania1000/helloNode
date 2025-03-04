f12c10646693d6e752b69257e189881471e69b77
Priyansh Sultania <psultania1000@gmail.com>
2025-03-04 18:16:51 +0530

    added ejs

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,10 @@ const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product.pug", { pageTitle: "Add product" });
+  res.render("add-product.pug", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+  });
 });
 
 // /admin/add-product => POST
