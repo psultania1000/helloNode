4bd1dc8c21945efc1a4e51966a95dc7e3bcdbf4b
Priyansh Sultania <psultania1000@gmail.com>
2025-03-06 05:40:38 +0530

    added controller in shop file

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,20 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
+const products = [];
+
 exports.postAddProduct = (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,10 @@ const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
 router.get("/add-product", productsController.getAddProduct);
 
 // /admin/add-product => POST
 router.post("/add-product", productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
