c080795bebc141a9b54393e995c0b6550add02e3
Priyansh Sultania <psultania1000@gmail.com>
2025-03-07 18:42:27 +0530

    storing data in file

--- models/product.js
+++ models/product.js
@@ -1,11 +1,19 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      console.log(fileContent);
+    });
   }
   static fetchAll() {
     return products;
