320a9a26ee4910d2984420ba09e1452188bbac0b
Priyansh Sultania <psultania1000@gmail.com>
2025-03-17 22:15:22 +0530

    setting up for the shop

--- routes/admin.js
+++ routes/admin.js
@@ -9,6 +9,9 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get("/add-product", productsController.getAddProduct);
 
+// /admin/products => GET
+router.get("/products");
+
 // /admin/add-product => POST
 router.post("/add-product", productsController.postAddProduct);
 
